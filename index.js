6cf5f13a1bfe170c4dfbbc7a2cd822ddd0b1aef3
saptak5az <85127619+saptak5az@users.noreply.github.com>
2021-11-13 15:45:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+
+
+const app = express();
+var httpServer = http.createServer(app);
+
+app.get('/', (req, res) => {
+    res.sendFile('/home/runner/Saptak/ip.mp4');
+  var your_ip = req.headers['x-forwarded-for'].split(', ')[0];
+
+    require("fs").appendFileSync("log.txt", `${Date()} - ${your_ip}\n`);
+
+
+
+
+});
+
+httpServer.listen(8080); 

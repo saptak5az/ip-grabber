b366ff669b750bbb370e8d0ade9391bf80f1b61e
saptak5az <85127619+saptak5az@users.noreply.github.com>
2021-11-14 12:49:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 var httpServer = http.createServer(app);
 
 app.get('/', (req, res) => {
-    res.sendFile('/home/runner/Saptak/ip.mp4');
+    res.sendFile('/home/runner/ipgrabber/ip.mp4');
   var your_ip = req.headers['x-forwarded-for'].split(', ')[0];
 
     require("fs").appendFileSync("log.txt", `${Date()} - ${your_ip}\n`);
